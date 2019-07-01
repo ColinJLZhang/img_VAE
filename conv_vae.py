adea70ea05be529c8444ff13795b0ec681fe3f75
mrobotor <mrobotor@gmail.com>
2019-07-01 16:11:02 +0800

    change dimension

--- conv_vae.py
+++ conv_vae.py
@@ -1,112 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2019-07-01 15:18:38
+# @Author  : mrobotor (colinzhang@applecore.cc)
+# @Link    : http://darklunar.ml
+# @Version : $Id$
+
+
 import tensorflow as tf
 from ops import *
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class ConvVAE(object):
-	'''Convolutional variational autoencoder'''
+    '''Convolutional variational autoencoder'''
 
-	def __init__(self, latent_dim, batch_size = 64):
-		
-		self.latent_dim = latent_dim
-		self.batch_size = batch_size
+    def __init__(self, latent_dim, batch_size=64):
+        self.latent_dim = latent_dim
+        self.batch_size = batch_size
 
-		# placeholder for input images. Input images are RGB 64x64
-		self.input_images = tf.placeholder(tf.float32, [None, 144, 256, 3])
-		input_images_flat = tf.reshape(self.input_images, [-1, 144*256*3])
+        # placeholder for input images. Input images are RGB 64x64
+        self.input_images = tf.placeholder(tf.float32, [None, 144, 256, 3])
+        input_images_flat = tf.reshape(self.input_images, [-1, 144 * 256 * 3])
 
-		# placeholder for z_samples. We are using this placeholder when we are generating new images
-		self.z_samples = tf.placeholder(tf.float32, [None, self.latent_dim])
+        # placeholder for z_samples. We are using this placeholder when we are generating new images
+        self.z_samples = tf.placeholder(tf.float32, [None, self.latent_dim])
 
-		# encoder
-		z_mean, z_logstd = self.encoder()
-		
-		# decoder input
-		samples = tf.random_normal([self.batch_size, self.latent_dim], 0, 1, dtype = tf.float32)
-		z = z_mean + (tf.exp(.5*z_logstd) * samples)
+        # encoder
+        z_mean, z_logstd = self.encoder()
 
-		# decoder
-		self.generated_images = self.decoder(z)
-		self.generated_images_sigmoid = tf.sigmoid(self.generated_images)
-		generated_images_flat = tf.reshape(self.generated_images, [-1, 144*256*3])
-		
-		# let's calculate the loss
-		'''
+        # decoder input
+        samples = tf.random_normal([self.batch_size, self.latent_dim], 0, 1, dtype=tf.float32)
+        z = z_mean + (tf.exp(.5 * z_logstd) * samples)
+
+        # decoder
+        self.generated_images = self.decoder(z)
+        self.generated_images_sigmoid = tf.sigmoid(self.generated_images)
+        generated_images_flat = tf.reshape(self.generated_images, [-1, 144 * 256 * 3])
+
+        # let's calculate the loss
+        '''
 		self.generation_loss = -tf.reduce_sum(input_images_flat * tf.log(1e-8 + generated_images_flat)\
 										 + (1 - input_images_flat) * tf.log(1e-8 + 1 - generated_images_flat), 1)'''
 
-		self.generation_loss = tf.reduce_sum(tf.maximum(generated_images_flat, 0) - generated_images_flat * input_images_flat\
-												 + tf.log(1 + tf.exp(-tf.abs(generated_images_flat))), 1)
+        self.generation_loss = tf.reduce_sum(
+            tf.maximum(generated_images_flat, 0) - generated_images_flat * input_images_flat \
+            + tf.log(1 + tf.exp(-tf.abs(generated_images_flat))), 1)
 
-		self.latent_loss = 0.5 * tf.reduce_sum(tf.square(z_mean) + tf.exp(2*z_logstd) - 2*z_logstd - 1, 1)
+        self.latent_loss = 0.5 * tf.reduce_sum(tf.square(z_mean) + tf.exp(2 * z_logstd) - 2 * z_logstd - 1, 1)
 
-		self.loss = tf.reduce_mean(self.generation_loss + self.latent_loss)
+        self.loss = tf.reduce_mean(self.generation_loss + self.latent_loss)
 
-		# and our optimizer
-		learning_rate = 1e-3
-		self.optimizer = tf.train.AdamOptimizer(learning_rate).minimize(self.loss)
+        # and our optimizer
+        learning_rate = 1e-3
+        self.optimizer = tf.train.AdamOptimizer(learning_rate).minimize(self.loss)
 
-		# generator for new frames
-		self.generator = self.decoder(self.z_samples, activation = tf.nn.sigmoid, reuse = True)
+        # generator for new frames
+        self.generator = self.decoder(self.z_samples, activation=tf.nn.sigmoid, reuse=True)
 
+    def encoder(self):
+        # first convolutional layer 144x256x3 -> 72x128x16
+        h1 = tf.nn.relu(conv2d(self.input_images, 3, 16, 'conv1'))
 
-	def encoder(self):
-		# first convolutional layer 144x256x3 -> 32x32x16
-		h1 = tf.nn.relu(conv2d(self.input_images, 3, 16, 'conv1'))
+        # second convolutional layer 72x128x16 -> 36x64x32
+        h2 = tf.nn.relu(conv2d(h1, 16, 32, 'conv2'))
 
-		# second convolutional layer 32x32x16 -> 16x16x32
-		h2 = tf.nn.relu(conv2d(h1, 16, 32, 'conv2'))
+        # fully connected layer 36x64x32 -> (-1, 36x64x32) -> (-1, 18x32x32)
+        h2_flat = tf.reshape(h2, [-1, 36 * 64 * 32])
+        h3_flat = dense(h2_flat, 36 * 64 * 32, 18 * 32 * 32, 'h3_flat_dense')
+        z_mean = dense(h3_flat, 18 * 32 * 32, self.latent_dim, 'z_mean_dense')
+        z_logstd = dense(h3_flat, 18 * 32 * 32, self.latent_dim, 'z_stddev_dense')
 
-		# fully connected layer
-		h2_flat = tf.reshape(h2, [-1, 16*16*32])
-		z_mean = dense(h2_flat, 16*16*32, self.latent_dim, 'z_mean_dense')
-		z_logstd = dense(h2_flat, 16*16*32, self.latent_dim, 'z_stddev_dense')
+        return z_mean, z_logstd
 
-		return z_mean, z_logstd
+    def decoder(self, z, activation=tf.identity, reuse=False):
+        # fully connected layer latent_dim -> 18x32x32
+        z_fc1 = dense(z, self.latent_dim, 18 * 32 * 32, 'z_fc_dense1', reuse)
+        # fully connected layer 18x32x32 => 36*64*32
+        z_fc2 = dense(z_fc1, 18 * 32 * 32, 36 * 64 * 32, 'z_fc_dense2', reuse)
 
-	def decoder(self, z, activation = tf.identity, reuse = False):
-		# fully connected layer
-		z_fc = dense(z, self.latent_dim, 16*16*32, 'z_fc_dense', reuse)
+        # first deconvolutional layer 36x64x32 -> 72x128x32
+        z_matrix = tf.nn.relu(tf.reshape(z_fc2, [-1, 36, 64, 32]))
+        h1 = tf.nn.relu(deconv2d(z_matrix, [self.batch_size, 72, 128, 32], 'deconv1', reuse=reuse))
 
-		# first deconvolutional layer 16x16x32 -> 32x32x16
-		z_matrix = tf.nn.relu(tf.reshape(z_fc, [-1, 16, 16, 32]))
-		h1 = tf.nn.relu(deconv2d(z_matrix, [self.batch_size, 32, 32, 16], 'deconv1', reuse))
+        # second deconvolutional layer 32x64x32 -> 72x128x16
+        h2 = tf.nn.relu(deconv2d(h1, [self.batch_size, 144, 256, 3], 'deconv2', reuse=reuse))
 
-		# second deconvolutional layer 32x32x16 -> 64x64x3
-		h2 = deconv2d(h1, [self.batch_size, 144, 256, 3], 'deconv2', reuse)
+        # third deconvolutional layer 32x64x32 -> 144x256x3 上采样
+        h3 = deconv2d(h2, [self.batch_size, 288, 512, 3], 'deconv3', reuse=reuse)
 
-		return activation(h2)
+        return activation(h2)
 
-	def training_step(self, sess, input_images):
-		sess.run(self.optimizer, feed_dict = {self.input_images:input_images})
+    def training_step(self, sess, input_images):
+        sess.run(self.optimizer, feed_dict={self.input_images: input_images})
 
-	def loss_step(self, sess, input_images):
-		return sess.run(self.loss, feed_dict = {self.input_images:input_images})
+    def loss_step(self, sess, input_images):
+        return sess.run(self.loss, feed_dict={self.input_images: input_images})
 
-	def generation_step(self, sess, z_samples):
-		'''Generates new images'''
-		return sess.run(self.generator, feed_dict = {self.z_samples:z_samples})
+    def generation_step(self, sess, z_samples):
+        '''Generates new images'''
+        return sess.run(self.generator, feed_dict={self.z_samples: z_samples})
 
-	def recognition_step(self, sess, input_images):
-		'''Reconstruct images'''
-		return sess.run(self.generated_images_sigmoid, feed_dict = {self.input_images:input_images})
+    def recognition_step(self, sess, input_images):
+        '''Reconstruct images'''
+        return sess.run(self.generated_images_sigmoid, feed_dict={self.input_images: input_images})
 
 
 if __name__ == '__main__':
+    # Let's test it before use
+    cvae = ConvVAE(2, batch_size=1)
+    init = tf.global_variables_initializer()
 
-	# Let's test it before use
-	cvae = ConvVAE(2, batch_size = 1)
-	init = tf.global_variables_initializer()
+    z_sample = np.random.normal(size=2)
 
-	z_sample = np.random.normal(size = 2)
+    print('z= {}'.format(z_sample))
 
-	print 'z= ', z_sample
-
-	with tf.Session() as sess:
-		sess.run(init)
-		output_frame = cvae.generation_step(sess, np.reshape(z_sample, [1, 2]))
-		output_frame = output_frame * 255
-		output_frame = output_frame.astype(np.uint8)
-		print 'Shape= ', output_frame.shape
-		plt.imshow(np.reshape(output_frame, [144, 256, 3]))
-		plt.show()
+    with tf.Session() as sess:
+        sess.run(init)
+        output_frame = cvae.generation_step(sess, np.reshape(z_sample, [1, 2]))
+        output_frame = output_frame * 255
+        output_frame = output_frame.astype(np.uint8)
+        print('Shape= ', output_frame.shape)
+        plt.imshow(np.reshape(output_frame, [144, 256, 3]))
+        plt.show()
--- ops.py
+++ ops.py
@@ -1,42 +1,47 @@
 import tensorflow as tf
 
-def conv2d(x, in_channels, output_channels, name, strides=[1,2,2,1], reuse = False):
-	'''Convolutional Layer'''
-	with tf.variable_scope(name,  reuse = reuse):
-		w = tf.get_variable('w', [5, 5, in_channels, output_channels], initializer = tf.truncated_normal_initializer(stddev = 0.1))
-		b = tf.get_variable('b', [output_channels], initializer = tf.constant_initializer(0.1))
 
-		conv = tf.nn.conv2d(x, w, strides = strides, padding = 'SAME') + b
-		return conv
+def conv2d(x, in_channels, output_channels, name, strides=[1, 2, 2, 1], reuse=False):
+    '''Convolutional Layer'''
+    with tf.variable_scope(name, reuse=reuse):
+        w = tf.get_variable('w', [5, 5, in_channels, output_channels],
+                            initializer=tf.truncated_normal_initializer(stddev=0.1))
+        b = tf.get_variable('b', [output_channels], initializer=tf.constant_initializer(0.1))
 
-def deconv2d(x, output_shape, name, strides = [1,2,2,1], reuse = False):
-	'''Deconvolutional Layer'''
-	with tf.variable_scope(name, reuse = reuse):
-		w = tf.get_variable('w', [5, 5, output_shape[-1], int(x.get_shape()[-1])], initializer = tf.truncated_normal_initializer(stddev = 0.1))
-		b = tf.get_variable('b', [output_shape[-1]], initializer = tf.constant_initializer(0.1))
+        conv = tf.nn.conv2d(x, w, strides=strides, padding='SAME') + b
+        return conv
 
-		deconv = tf.nn.conv2d_transpose(x, w, output_shape = output_shape, strides = strides) + b
-		return deconv
 
-def dense(x, input_dim, output_dim, name, reuse = False):
-	'''Fully-connected Layer'''
-	with tf.variable_scope(name, reuse = reuse):
-		w = tf.get_variable('w', [input_dim, output_dim], initializer = tf.truncated_normal_initializer(stddev = 0.1))
-		b = tf.get_variable('b', [output_dim], initializer = tf.constant_initializer(0.1))
+def deconv2d(x, output_shape, name, strides=[1, 2, 2, 1], reuse=False):
+    '''Deconvolutional Layer'''
+    with tf.variable_scope(name, reuse=reuse):
+        w = tf.get_variable('w', [5, 5, output_shape[-1], int(x.get_shape()[-1])],
+                            initializer=tf.truncated_normal_initializer(stddev=0.1))
+        b = tf.get_variable('b', [output_shape[-1]], initializer=tf.constant_initializer(0.1))
 
-		return tf.matmul(x, w) + b
+        deconv = tf.nn.conv2d_transpose(x, w, output_shape=output_shape, strides=strides) + b
+        return deconv
+
+
+def dense(x, input_dim, output_dim, name, reuse=False):
+    '''Fully-connected Layer'''
+    with tf.variable_scope(name, reuse=reuse):
+        w = tf.get_variable('w', [input_dim, output_dim], initializer=tf.truncated_normal_initializer(stddev=0.1))
+        b = tf.get_variable('b', [output_dim], initializer=tf.constant_initializer(0.1))
+
+        return tf.matmul(x, w) + b
 
 
 if __name__ == '__main__':
-	'''test all ops'''
-	x1 = tf.random_normal([1,9,9,3])
-	c1 = conv2d(x1, 3, 5, "c1")
-	dc1 = deconv2d(c1, [1,9,9,3], "decon1")
-	x2 = tf.random_normal([1,9])
-	d1 = dense(x2, 9, 5, "dense1")
-	
-	print("x1:", x1)
-	print("c1:", c1)
-	print("dc1:", dc1)
-	print("x2:", x2)
-	print("d1:", d1)
+    '''test all ops'''
+    x1 = tf.random_normal([1, 9, 9, 3])
+    c1 = conv2d(x1, 3, 5, "c1")
+    dc1 = deconv2d(c1, [1, 9, 9, 3], "decon1")
+    x2 = tf.random_normal([1, 9])
+    d1 = dense(x2, 9, 5, "dense1")
+
+    print("x1:", x1)
+    print("c1:", c1)
+    print("dc1:", dc1)
+    print("x2:", x2)
+    print("d1:", d1)
